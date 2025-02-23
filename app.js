594183419a79f20d1ff89e689b4650951d45d41f
mhonate <marcelo.onate@gmail.com>
2025-02-22 21:09:55 -0300

    actualizar el array y sortear al amigo

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let amigos = [];
 function agregarAmigo() {
     
     const ingresarAmigo = document.getElementById('amigo');
-    const nombreAmigo = ingresarAmigo.value.trim()
+    const nombreAmigo = ingresarAmigo.value.trim();
 
 
 // 2.- Validar la entrada
@@ -24,4 +24,44 @@ if (nombreAmigo ===''){
     ingresarAmigo.value = '';
     actualizaListaAmigos();
 }
+
+
+//3.- Agregar y actualizar el array de amigos
+
+function actualizaListaAmigos() {
+    const listaAmigos = document.getElementById('listaAmigos');
+
+     //4.-Limpiar el campo de entrada
+    listaAmigos.innerHTML ='';
+
+    amigos.forEach((amigos, index) => {
+        const li = document.createElement('li');
+        li.innerHTML = `${index + 1}.- ${amigos}`;
+        listaAmigos.appendChild(li);
+    });
+}
+
+
+//4.- Sortear amigos
+
+function sortearAmigo(){
+    //5.-Validar que haya amigos en la lista
+    if (amigos.length == 0){
+        alert('No hay amigos en la lista');
+        return;
+    }
+    
+    //6.-Generar un índice aleatorio
+    const indiceAmigo = Math.floor(Math.random() * amigos.length );
+
+    //7.-Obtener el nombre sorteado
+    const amigoSorteado = amigos[indiceAmigo];
+
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `El amigo sorteado es:  ${amigoSorteado}!!!`;
+    
+
+}
+
 
